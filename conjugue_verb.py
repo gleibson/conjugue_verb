cc8dcc4f73a4ad97aaf69772c5a745c5f7a7d0e3
glashunti <gleibsonglas@gmail.com>
2016-08-01 15:59:24 -0300

    Update conjugue_verb.py
    access the wiktionary to confirm if the portugues Brazil verb is right, wrong and if the verb exist on the wiktionary website

--- conjugue_verb.py
+++ conjugue_verb.py
@@ -5,9 +5,16 @@
 #carrega a biblioteca de funçoes de acesso e manipulaçao de dados
 import MySQLdb
 #informa os dados da conexao 
-dados = MySQLdb.connect(host="*******", port=*****, user="*******", passwd="*****", db="******") 
+dados = MySQLdb.connect(host='****', port=*****, user='******', passwd='*****', db='******') 
 # cursor de manipulaçao   
 cobra = dados.cursor()
+#Informações relativas à conexão com a web
+# carrega a biblioteoca de funções para o acesso webb
+import urllib2
+#usuario
+print '===================================================='
+print 'Programa conjugador de verbos regulares em portugues'
+print '===================================================='
 #informaçoes do usuario
 seu_nome = raw_input("Digite seu primeiro nome: ")
 seu_sobrenome = raw_input("Digite seu sobrenome: ")
@@ -27,6 +34,23 @@ print 'Conjugarei um verbo regular para você'
 print
 #O usuario informa o verbo
 seu_verbo = raw_input("Digite o infinitivo de um verbo regular:") #verbo a se conjugado
+#Consulta no wikcionario 
+print 'Aguarde um momento enquanto consulto o Wikicionario para saber'
+print 'se o verbo' + seu_verbo + 'consta como verbo regular'
+print
+#endereço com o verbo a ser consultado
+endereco_web = 'http://pt.wiktionary.org/wiki/' + seu_verbo
+# depois, cria a requisição para a busca
+req_web = urllib.Request(endereco_web, headers={'User-Agent': "Mozilla5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"})
+# busca conteudo
+conteudo_web = urllib2.urlopen(req_web)
+# armazena conteudo
+conteudo_web = conteudo_web.read()
+#pesquisa se o verbo é regular
+if ("Verbo regular da "in conteudo_web):
+  print 'Confirmado: ' + seu_verbo + ' é um verbo regular!'
+  print 'Verificarei se ja foi conjugado anteriormente.'
+  print 
 #existencia do verbo dentro da base de dados
 existe = cobra.execute(("SELECT verbo FROM verbo WHERE verbo=s%"), (seu_verbo))
 if existe > 0:
@@ -38,6 +62,7 @@ if existe > 0:
  dados.commit()
  print 'Esse verbo ja foi anteriormente conjugado: ' + seu_verbo
 else:
+ print 'Essa é a primeira vez que conjugo '+ seu_verbo + ' !'
  cobra.execute("INSERT INTO verbo (verbo, frequencia, regular) VALUES(s%, s% s%)", (seu_verbo,1,1))
  dados.commit()
 termina_em = seu_verbo[-2:] #separa a terminacao do verbo
@@ -56,4 +81,11 @@ elif termina_em == 'ir':
 
 else:
   print 'Tem certeza que '+seu_verbo+' é um verbo regular?'
+  print 
+  print 'O Wikicionário é um projeto colaborativo e pode ser que ' + seu_verbo
+  print 'ainda não conste nele. Se for esse o caso, visite a página web'
+  print 'htttp://pt.wiktionary.org e complemente a informação sobre ' + seu_verbo + '.'
+  print
+  print 'Obrigado'
+  print 
 dados.close()
